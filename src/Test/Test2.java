53e211bb23f14ee8552c969a3be04ff4353ec925
lukasblunck96 <49271087+lukasblunck96@users.noreply.github.com>
2019-04-04 12:43:03 +0200

    Große Welt
    dsfasf

--- src/Test/Test2.java
+++ src/Test/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hallo Welt");
+System.out.println("Hallo große Welt");
 	}
 
 }
