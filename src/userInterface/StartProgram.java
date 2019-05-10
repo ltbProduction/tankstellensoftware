9416b4bbb4fdfc4047b3d2ea90984e8c66ab3cab
birkhofer_r <birkhofer_r@winrb2018.topflow.local>
2019-05-10 18:46:01 +0200

    UserInterface pushen

---
+++ src/userInterface/StartProgram.java
@@ -0,0 +1,30 @@
+package userInterface;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartProgram extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void init() {
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("UserInterface2.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Tankstellensoftware");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+	public void stop() {
+	}
+	
+}
