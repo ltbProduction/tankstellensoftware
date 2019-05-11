350351c9d79e5b42db1c9410ce4de802141e19de
lukas <lukas@LAPTOP-B8H24SRR.fritz.box>
2019-05-11 11:55:39 +0200

    Controller geändert

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,10 +1,29 @@
 package controller;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.image.Image;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Controller implements EventHandler<ActionEvent> {
 
+	private View view;
+
+	public Controller(View view) {
+		this.view = view;
+	}
+
+	@Override
+	public void handle(ActionEvent actionEvent) {
+		if (actionEvent.getSource().equals(view.getButton())) {
+			System.out.println("Drucktaste 1 betätigt");
+			String text = view.getTextField().getText();
+			view.getLabel().setText(text);
+			
+		} else if (actionEvent.getSource().equals(view.getButton2())) {
+			System.out.println("Drucktaste 2 betätigt");
+			String text = view.getTextField().getText();
+			view.getLabel2().setText(text);
+		}
 	}
 
 }
