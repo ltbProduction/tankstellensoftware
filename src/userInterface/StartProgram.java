afed69062f5cdeb0339ea909327ca442b519ec3b
lukasblunck96 <49271087+lukasblunck96@users.noreply.github.com>
2019-05-11 17:50:05 +0200

    Controller mit fx:ids und den meisten Methoden. Es fehlen noch die kleineren Popup-Fenster.

--- src/userInterface/StartProgram.java
+++ src/userInterface/StartProgram.java
@@ -4,27 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+//import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class StartProgram extends Application{
+public class StartProgram extends Application {
 
 	public static void main(String[] args) {
-		launch(args);
+		Application.launch(args);
 	}
-	
+
 	public void init() {
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("UserInterface.fxml"));
-        Scene scene = new Scene(root);
-        primaryStage.setTitle("Tankstellensoftware");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+
+		Parent root = FXMLLoader.load(getClass().getResource("UserInterface.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Tankstellensoftware");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
 	public void stop() {
 	}
-	
+
 }
