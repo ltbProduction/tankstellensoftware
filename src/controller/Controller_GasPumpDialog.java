9bffbee92564420c9e2d4b8161024d51cf754249
lukasblunck96 <49271087+lukasblunck96@users.noreply.github.com>
2019-05-20 18:10:37 +0200

    Clean Up

--- src/controller/Controller_GasPumpDialog.java
+++ src/controller/Controller_GasPumpDialog.java
@@ -9,9 +9,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import models.FuelTank;
-import models.GasStation;
 
 public class Controller_GasPumpDialog implements Initializable {
 	
